b80ed19c77601fafef398cafa2e4099e77ce4f45
JonnyG511 <35736713+JonnyG511@users.noreply.github.com>
2019-01-12 16:35:27 -0500

    1st robot program
    This is generally  the base of the robot.

--- robot/robot.py
+++ robot/robot.py
@@ -1,4 +1,24 @@
 #!/usr/bin/env python3
-#
-# TODO: Add robot code below
 
+import wpilib
+from magicbot import MagicRobot
+
+
+class MyRobot(MagicRobot):
+
+    #
+    # Define components here
+    #
+
+    def createObjects(self):
+        """Initialize all wpilib motors & sensors"""
+
+        self.joystick = wpilib.Joystick(0)
+
+    def teleopPeriodic(self):
+        """Place code here that does things as a result of operator
+           actions"""
+
+
+if __name__ == "__main__":
+    wpilib.run(MyRobot)
