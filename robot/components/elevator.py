3daace7bca55e718d32fcb79e059321ab6e009c4
JimmieB <35736646+JimmieB@users.noreply.github.com>
2019-01-19 11:25:37 -0500

    Merge branch 'master' of https://github.com/frc6367/2019-robot

--- robot/components/elevator.py
+++ robot/components/elevator.py
@@ -2,5 +2,30 @@ import ctre
 
 
 class Elevator:
+
+    motor1: ctre.WPI_TalonSRX
+    motor2: ctre.WPI_TalonSRX
+
+    def setup(self):
+        self.motor2.follow(self.motor1)
+
+    ## Go to specific position
+    def goToPosition(self, position):
+        self.position = position
+
+    def goToTop(self):
+        self.goToPosition(1)
+
+    def goToMid(self):
+        self.goToPosition(2)
+
+    def gotToBottom(self):
+        self.goToPosition(3)
+
+    ## STOP
+    def stop(self):
+        pass
+
+    ## Incremental up and down
     def execute(self):
         pass
