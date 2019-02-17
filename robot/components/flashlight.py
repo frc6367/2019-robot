e202ffa7825e50007b40d29105999e47a0ed9c72
JonnyG511 <35736713+JonnyG511@users.noreply.github.com>
2019-02-17 09:33:10 -0500

    Update flashlight.py

--- robot/components/flashlight.py
+++ robot/components/flashlight.py
@@ -1,9 +1,20 @@
-# class limelight:
-#     def setup():
-#         if self.camera_enabled:
-#             data = self.vision_sim.compute(now, x, y, angle)
-#         if data is not None:
-#             self.nt.putNumberArray('/camera/target', data[0])
-#     else:
-#         self.vision_sim.dont_compute()
-    
+float Kp = -0.1f;
+float min_command = 0.05f;
+
+std::shared_ptr<NetworkTable> table = NetworkTable::GetTable("limelight");
+float tx = table->GetNumber("tx");
+
+if (joystick->GetRawButton(9))
+{
+        float heading_error = -tx;
+        float steering_adjust = 0.0f;
+        if (tx > 1.0)
+        {
+                steering_adjust = Kp*heading_error - min_command;
+        }
+        else if (tx < 1.0)
+        {
+                steering_adjust = Kp*heading_error + min_command;
+        }
+        left_command += steering_adjust;
+        right_command -= steering_adjust;
